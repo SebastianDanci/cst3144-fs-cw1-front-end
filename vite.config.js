ef2f5f6f4a38a5ae91f37e5a3dfe61f60a60d5fe
Sebastian Robert Danci <ir3kt4fun@gmail.com>
2025-11-23 13:29:55 +0000

    Revert "Updated Base Path For Github Pages Deployment"
    This reverts commit 8aff8091cc65d3290d2b821a9247b95f4fec911a.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/cst3144-fs-cw1-front-end/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
